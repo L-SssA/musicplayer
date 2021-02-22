46a93a758c04ba49463b4de9fee91df899f85e05
Lsssan <1091411975@qq.com>
2021-02-22 09:36:09 +0800

    feat: 设置路径别名

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
   publicPath: './',
   devServer: {
     proxy: 'http://localhost:3000'
+  },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("@", resolve('src'))
+      .set("~c", resolve('src/components/'))
+
   }
 }
