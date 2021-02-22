94565384e23105b70f1f205d74d1099d38f7f3ac
Lsssan <1091411975@qq.com>
2021-02-22 09:34:53 +0800

    refactor: 提取字幕滚动组件

---
+++ src/components/LyricBox.vue
@@ -0,0 +1,36 @@
+<template>
+  <p
+    v-for="(item, index) in lyric"
+    :key="index"
+    :class="{ active: index === lineIndex }"
+    class="lyric-line"
+  >
+    {{ item.text }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    lyric: {
+      type: Array,
+      default: () => [],
+    },
+    lineIndex: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.lyric-line {
+  text-align: center;
+  transition: font-weight 0.3s ease-in-out, font-size 0.3s ease-in-out;
+}
+.active {
+  font-weight: 900;
+  font-size: 1.6rem;
+}
+</style>
